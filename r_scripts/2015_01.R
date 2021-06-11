a8d5cfc714e27d2165814c5272d498404df0546f
Abby Walter <lawalter@mymail.vcu.edu>
2021-06-11 15:33:02 -0400

    Add files via upload

--- r_scripts/2015_01.R
+++ r_scripts/2015_01.R
@@ -1,13 +1,28 @@
 library(tidyverse)
 
-directions <- read_file("data/2015_01.txt")
+raw_directions <- read_file("data/2015_01.txt")
 
-directions %>% 
+# part 1 ------------------------------------------------------------------
+
+directions <-
+  raw_directions %>% 
   str_replace_all("\\(", "1\\,") %>% 
   str_replace_all("\\)", "-1\\,") %>% 
   str_remove("\\,$") %>% 
   str_split("\\,") %>% 
   as_vector() %>% 
-  as.numeric() %>% 
-  sum()
+  as.numeric()
+
+sum(directions)
+
+# part 2 ------------------------------------------------------------------
+
+map_dbl(
+  1:7000, 
+  ~sum(directions[1:.x])) %>% 
+  as_tibble() %>% 
+  mutate(floor = row_number()) %>% 
+  filter(value < 0) %>% 
+  filter(floor == min(floor)) %>% 
+  pull(floor)
 
