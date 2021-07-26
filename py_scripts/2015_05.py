854bd8b20e7c1407be166f95ec363bfa77faec66
Abby Walter <lawalter@mymail.vcu.edu>
2021-07-26 13:34:18 -0400

    completed 2015 week 5

--- py_scripts/2015_05.py
+++ py_scripts/2015_05.py
@@ -15,14 +15,14 @@ with open('data/2015_05.txt') as f:
 
 # A nice string is one with all of the following properties:
 
-# It contains at least three vowels (aeiou only)
-# It contains at least one letter that appears twice in a row
-# It does not contain the strings ab, cd, pq, or xy
+# 1) It contains at least three vowels (aeiou only)
+# 2) It contains at least one letter that appears twice in a row
+# 3) It does not contain the strings ab, cd, pq, or xy
 
 nice = []
 
 for i in range(len(strings)):
-    # Nice if doesn't contain ab, cd, pq, or xy AND contains >= 3 vowels AND has a letter that is repeated 2x in a row
+    # Nice if rules 1 AND 2 AND 3 are true
     if not re.search('ab|cd|pq|xy', strings[i]) and re.search(r'[aeiou].*[aeiou].*[aeiou]', strings[i]) and re.search(r'((\w)\2)', strings[i]): 
         nice.append(strings[i])
     else:
@@ -31,23 +31,25 @@ for i in range(len(strings)):
 print(nice)
 print(len(nice))
 
-# Answer 255
+# Answer: 255
     
 #%% Week 5, part 2
 
 # Now, a nice string is one with all of the following properties:
 
-# It contains a pair of any two letters that appears at least twice in the string without overlapping
-# It contains at least one letter which repeats with exactly one letter between them
+# 1) It contains a pair of any two letters that appears at least twice in the string without overlapping
+# 2) It contains at least one letter which repeats with exactly one letter between them
 
 
 nice2 = []
 
 for i in range(len(strings)):
-    if re.search(r'', strings[i]) and re.search(r'((\w)\2)', strings[i]): 
+    if re.search(r'(\w\w).*(\1)', strings[i]) and re.search(r'((\w).{1}\2)', strings[i]): 
         nice2.append(strings[i])
     else:
         None      
         
 print(nice2)
 print(len(nice2))
+
+# Answer: 55
